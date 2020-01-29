e0c0a61dc65de2313e17425b070e8320eb85c095
nryzhenkov <nryzhenkov@yandex.ru>
2020-01-29 19:43:18 +0300

    First commit

---
+++ AnotherSimCore/Cable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AnotherSimCore
+{
+    public class Cable
+    {
+        Node firstNode;
+        Node secondNode;
+        public bool isAlive { get; private set; }
+        public Cable(Node first, Node second) {
+            firstNode = first;
+            secondNode = second;
+            isAlive = true;
+        }
+    }
+}
---
+++ AnotherSimCore/Node.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AnotherSimCore
+{
+    public class Node
+    {
+    }
+}
